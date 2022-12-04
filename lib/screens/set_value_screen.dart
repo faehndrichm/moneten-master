ccf6dc90805bf3632068b079a8229c365b82515c
Dominik Fenzl <e1526544@student.tuwien.ac.at>
2022-12-04 16:09:30 +0100

    Implemented Set Value for day
    The user can now enter a custom value to set the value for the day.

---
+++ lib/screens/set_value_screen.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:simple_budget/providers.dart';
+
+import '../helper.dart';
+
+class SetValueScreen extends ConsumerWidget {
+  const SetValueScreen({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final valueController = TextEditingController();
+    final currentDate = ref.read(dayProvider).currentDate;
+
+    valueController.text = "${ref.read(cashProvider).cashPerDay[toDateString(currentDate)]}";
+
+    String selectedDate = toDateStringUI(currentDate);
+
+    final textField = TextField(
+        style: const TextStyle(fontSize: 28),
+        controller: valueController,
+        keyboardType: TextInputType.number,
+        decoration: InputDecoration(
+          border: const OutlineInputBorder(),
+          labelText: 'Value for $selectedDate',
+        ),
+        onSubmitted: (value) {
+          int? amount = int.tryParse(value);
+          if (amount != null) {
+            ref.read(cashProvider.notifier).setValue(ref.read(dayProvider).currentDate, amount);
+            Navigator.pop(context);
+          } else {
+            valueController.text =
+                ref.read(limitProvider).value.toString(); //reset
+            ScaffoldMessenger.of(context).showSnackBar(
+              SnackBar(
+                duration: const Duration(seconds: 20),
+                action: SnackBarAction(
+                  label: 'OK',
+                  onPressed: () {},
+                ),
+                content: const Text("Na das geht aber nicht!"),
+              ),
+            );
+          }
+        });
+
+    return Scaffold(
+        appBar: AppBar(
+          centerTitle: true,
+          elevation: 0,
+          backgroundColor: Colors.lightGreen,
+          title: Text(
+            "Daily Spent",
+            style: TextStyle(
+                fontFamily: 'Poppins',
+                fontSize: 28,
+                color: Colors.grey.shade700),
+          ),
+        ),
+        body: Center(child: textField));
+  }
+}
